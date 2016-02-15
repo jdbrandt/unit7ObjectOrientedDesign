22d93bcf2bdfeb4e83a3b1ac45ac5d903fdc2bd1
jdbrandt <jdbrandt@stu.naperville203.org>
2016-02-14 23:43:02 -0600

    FINISHED THE TRIANGLE

--- chapter7practice/TriangleGUIComponent.java
+++ chapter7practice/TriangleGUIComponent.java
@@ -6,6 +6,8 @@ public class TriangleGUIComponent extends JComponent
 {
     
     
+    private static final double R = 12;
+    
     private Line2D.Double line1;
     private Line2D.Double line2;
     private Line2D.Double line3;
@@ -38,25 +40,49 @@ public class TriangleGUIComponent extends JComponent
         g2.draw(line1);
         g2.draw(line2);
         g2.draw(line3);
+        g2.draw(ellipse1);
+        g2.draw(ellipse2);
+        g2.draw(ellipse3);
     }
     
-    public void setLine1(int x1, int y1, int x2, int y2)
+    public void setPoint1(double x, double y)
     {
-        line1.setLine(x1, y1, x2, y2);
+        point1.setLocation(x,y);
+        ellipse1.setFrame(x-R, y-R, 2*R, 2*R);
         repaint();
     }
     
-    public void setLine2(int x1, int y1, int x2, int y2)
+    public void setPoint2(double x, double y)
     {
-        line2.setLine(x1, y1, x2, y2);
+        point2.setLocation(x,y);
+        ellipse2.setFrame(x-R, y-R, 2*R, 2*R);
+        line1.setLine(point1.getX(), point1.getY(), x, y);
         repaint();
     }
     
-    public void setLine3(int x1, int y1, int x2, int y2)
+    public void setPoint3(double x, double y)
     {
-        line3.setLine(x1, y1, x2, y2);
+        point3.setLocation(x,y);
+        ellipse3.setFrame(x-R, y-R, 2*R, 2*R);
+        line2.setLine(point2.getX(), point2.getY(), x, y);
+        line3.setLine(x, y, point1.getX(), point1.getY());
         repaint();
     }
+    
+    public void clear()
+    {
+        point1.setLocation(0,0);
+        point2.setLocation(0,0);
+        point3.setLocation(0,0);
+        line1.setLine(0,0,0,0);
+        line2.setLine(0,0,0,0);
+        line3.setLine(0,0,0,0);
+        ellipse1.setFrame(0,0,0,0);
+        ellipse2.setFrame(0,0,0,0);
+        ellipse3.setFrame(0,0,0,0);
+        repaint();
+    }
+    
     
 }
     
--- chapter7practice/TriangleGUIFrame.java
+++ chapter7practice/TriangleGUIFrame.java
@@ -1,18 +1,34 @@
 import java.awt.event.*;
 import javax.swing.*;
 
-public class TraingleGUIFrame extends JFrame
+public class TriangleGUIFrame extends JFrame
 {
     private static final int FRAME_WIDTH = 600;
     private static final int FRAME_HEIGHT = 600;
     
     private TriangleGUIComponent scene;
     
+    
+    
+    public TriangleGUIFrame()
+    {
+        scene = new TriangleGUIComponent();
+        add(scene);
+        
+        MouseListener listener = new MousePressListener();
+        scene.addMouseListener(listener);
+        
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+    }
+    
+    
+    
+    
+    
     class MousePressListener implements MouseListener
     {
         private int clicks;
         
-        private int x1, y1, x2, y2, x3, y3;
         
         public MousePressListener()
         {
@@ -25,30 +41,35 @@ public class TraingleGUIFrame extends JFrame
             
             if (clicks == 1)
             {
-                x1 = event.getX();
-                y1 = event.getX();
+                scene.setPoint1(event.getX(), event.getY());
             }
             
             else if (clicks == 2)
             {
-                x2 = event.getX();
-                y2 = event.getY();
-                
+                scene.setPoint2(event.getX(), event.getY());
             }
             else if (clicks == 3)
             {
-                x3 = event.getX();
-                y3 = event.getY();
+                scene.setPoint3(event.getX(), event.getY());
             }
             else if (clicks == 4)
             {
+                scene.clear();
                 clicks = 0;
-                x1 = 0; y1 = 0; x2 = 0; y2 = 0; x3 = 0; y3 = 0;
             
             }
         }
         
+        public void mouseReleased(MouseEvent event) {};
+        public void mouseClicked(MouseEvent event) {};
+        public void mouseEntered(MouseEvent event) {};
+        public void mouseExited(MouseEvent event) {};
     }
-}
+    
+    
+    
+    
+    }
+
 
             
